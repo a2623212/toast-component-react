af5d04224755c37546666a2b0184d9fa27847866
a2623212 <a2623212@gmail.com>
2023-07-02 15:20:49 +0900

    feat: Live-editable toast preview.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -9,6 +10,11 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isShowed, setIsShowed] = React.useState(false);
+
+  function handleDismiss() {
+    setIsShowed(false);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -16,7 +22,11 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-
+      {isShowed && (
+        <Toast variant={variant} handleDismiss={handleDismiss}>
+          {message}
+        </Toast>
+      )}
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -71,7 +81,7 @@ function ToastPlayground() {
           >
             <Button
               onClick={() => {
-                window.alert(`${variant}-${message}`);
+                setIsShowed(true);
               }}
             >
               Pop Toast!
